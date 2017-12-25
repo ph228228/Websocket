ae165b3989335878918c331ac1e125e0a4d457b2
penghao <869279342@qq.com>
2017-12-25 17:41:48 +0800

    加入了websocket通讯，实现了不同房间内的消息交流

--- src/main/java/com/example/beans/MyWebsocket.java
+++ src/main/java/com/example/beans/MyWebsocket.java
@@ -1,6 +1,5 @@
 package com.example.beans;
 
-
 import net.sf.json.JSONObject;
 
 import java.io.IOException;
@@ -9,9 +8,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArraySet;
-
-
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -20,45 +16,43 @@ import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
-
-
-
 //每个客户端连接成功的时候在后台都会创建一个相应的MyWebsocket类
 @ServerEndpoint("/websocket/{roomCode}")
 public class MyWebsocket {
 
-    private static CopyOnWriteArraySet<MyWebsocket> websocketPools=new CopyOnWriteArraySet<MyWebsocket>();
+    //private static CopyOnWriteArraySet<MyWebsocket> websocketPools=new CopyOnWriteArraySet<MyWebsocket>();
 
     //concurrent包的线程安全Map，用来存放每个客户端对应的MyWebSocket对象。其中key为房间号标识
-    private static Map<String,List<MyWebsocket>> sessionPools=new ConcurrentHashMap();
+    private static Map<String, List<MyWebsocket>> sessionPools = new ConcurrentHashMap();
 
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
 
     //当前客户所在房间的房间号
     private String roomCode;
+
     /**
      * 连接建立成功调用的方法
+     *
      * @param session  可选的参数。session为与某个客户端的连接会话，需要通过它来给客户端发送数据
+     * @param roomCode 房间号
      */
     @OnOpen
-    public void onOpen(@PathParam("roomCode")String roomCode, Session session)
-    {
-        this.session=session;
+    public void onOpen(@PathParam("roomCode") String roomCode, Session session) {
+        this.session = session;
         //将连接地址的参数roomcode的值放入变量roomCode中
-        this.roomCode=roomCode;
-        if (sessionPools.containsKey(this.roomCode)){
+        this.roomCode = roomCode;
+        if (sessionPools.containsKey(this.roomCode)) {
             sessionPools.get(this.roomCode).add(this);
-        }else {
-            sessionPools.put(this.roomCode,new ArrayList<MyWebsocket>());
+        } else {
+            sessionPools.put(this.roomCode, new ArrayList<MyWebsocket>());
             sessionPools.get(this.roomCode).add(this);
         }
 
     }
 
     @OnClose
-    public void onClose()
-    {
+    public void onClose() {
         sessionPools.get(roomCode).remove(this);
         try {
             this.session.close();
@@ -73,11 +67,11 @@ public class MyWebsocket {
         JSONObject jsonObject = JSONObject.fromObject(message);
         //遍历map集合，将消息发送至同一个房间下的session
         Iterator<Map.Entry<String, List<MyWebsocket>>> iterator = sessionPools.entrySet().iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             Map.Entry<String, List<MyWebsocket>> entry = iterator.next();
-            if (entry.getKey().equals(this.roomCode)){
-                //判断为同一个房间，则遍历房间内的session，并发送消息
-                for (MyWebsocket item:entry.getValue()){
+            if (entry.getKey().equals(this.roomCode)) {
+                //判断若是为同一个房间，则遍历房间内的session，并发送消息
+                for (MyWebsocket item : entry.getValue()) {
                     jsonObject.put("isSelf", item.equals(session));
                     try {
                         // 添加本条消息是否为当前会话本身发的标志
@@ -91,20 +85,15 @@ public class MyWebsocket {
         }
     }
 
-
-    private void send(String message) throws IOException{
+    private void send(String message) throws IOException {
         this.session.getAsyncRemote().sendText(message);
 //this.session.getBasicRemote().sendText(message);
     }
 
-
     @OnError
-    public void onError(Session session, Throwable error)
-    {
+    public void onError(Session session, Throwable error) {
         System.out.println("发生错误");
         error.printStackTrace();
     }
-
-
 
 }
